cea6d9e979ef205eecb9235ab38894bfb599832b
Caleb Rodriguez <sirdigo6@gmail.com>
2021-11-23 21:41:19 -0500

    commit tree b1ebaf7e62eb9f59c9a2595adbc61c87a1c044b6

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toLocaleUpperCase();
+    return customerName;
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'stacy';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'karen';
+    return leastFavoriteCustomer;
+}
